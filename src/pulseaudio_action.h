d3f03d6e3f3f6d6453046e06fae8451f02576a9e
Michael Niewoehner <mniewoeh@stud.hs-offenburg.de>
2016-05-13 20:58:04 +0200

    Add an option to sync the sink of all input streams when the default sink is changed

--- src/pulseaudio_action.h
+++ src/pulseaudio_action.h
@@ -32,6 +32,9 @@ void pulseaudio_move_input_to_sink(menu_info_item_t* input, menu_info_item_t* si
 void pulseaudio_move_output_to_source(menu_info_item_t* output, menu_info_item_t* source);
 void pulseaudio_move_success_cb(pa_context *c, int success, void *userdata);
 
+void pulseaudio_move_all_inputs_to_sink(menu_info_item_t* sink);
+void pulseaudio_move_all_inputs_to_sink_cb(pa_context* c, const pa_sink_input_info* i, int is_last, void* userdata);
+
 void pulseaudio_rename(menu_info_item_t* mii, const char* name);
 void pulseaudio_rename_success_cb(pa_context *c, int success, void *userdata);
 
